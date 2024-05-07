052dc676d43ccdca0c699b564da441cd8e27f65f
Gabriel Scarafia <49571740+GabScarafia@users.noreply.github.com>
2024-05-06 21:14:44 -0300

    Mi "IA" ya identifica clases de imagenes
    Habria que ir agregando las otra imagenes

--- football_kit_recognizer.py
+++ football_kit_recognizer.py
@@ -4,11 +4,51 @@ os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
 import tensorflow as tf
 import pathlib
-#OS commands
-os.system('cls')
+from PIL import Image
 
-# Con esto lo que estoy intentando es tener el repositorio de imagenes a mano.
-data_dir = pathlib.Path('Imagenes')
-#list_ds = tf.data.Dataset.list_files(str(data_dir/'*/*.jpg'))
-image_count = len(list(data_dir.glob('*/*.jpg')))
-print("Cantidad de imagenes del directorio: "+ str(image_count))
+#region Basico
+#Lo agrego a una funcion no pq sea necesario sino por si despues necesito definir funciones extras
+#esas funciones extras tambien podrian ir en otros archivos. (a todo caso lo cambiamos)
+def main():
+    # Con esto lo que estoy intentando es tener el repositorio de imagenes a mano.
+    data_dir = pathlib.Path('Imagenes')
+    # Para mas tarde )? -Gaby
+    list_dataset = tf.data.Dataset.list_files(str(data_dir/'*/*.jpg'))
+    #Cuento e imprimo
+    image_count = len(list(data_dir.glob('*/*.jpg')))
+    print("------------------------------------------")
+    print("Cantidad de imagenes del directorio: "+ str(image_count))
+    print("------------------------------------------")
+
+    #Reconocer grupo de imagenes
+    river = list(data_dir.glob('River/*'))
+    boca = list(data_dir.glob('Boca/*'))
+    image = Image.open(str(river[0]))
+    #Abre la imagen que reconocio: 
+    #no se que tan util es excepto para saber en donde estamos parado...
+    #image.show()
+#endregion
+
+#region Proceso
+    #Definicion de datos para entrenar
+    batch_size = 32
+    img_height = 180 #Qsy
+    img_width = 180
+    #Definicion de entrenar
+    train_dataset = tf.keras.utils.image_dataset_from_directory(
+        data_dir,
+        validation_split=0, #esto va 0 pq creo que nosotros vamos a darle input cuando vayamos a validar
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size)
+    
+    #con esto deberia ver los N directorios que podriamos (serian nuestras clases)
+    class_names = train_dataset.class_names
+    print("----------------------------")
+    print("Clases que identifico:")
+    print(class_names)
+    print("----------------------------")
+#endregion
+
+#Corro el codigo:
+main()
--- readme.txt
+++ readme.txt
@@ -3,4 +3,11 @@ Video de Guia: https://www.youtube.com/watch?v=CahXWhovhKA
 1.Instalar Python: https://www.python.org/downloads/
 2.Fijarse las variables de entorno. (seguir el video)
 3.pip install tensorflow --upgrade en la terminal de visual studio (o en la carpeta del proyecto)
+3b. (tuve que darle permisos de administrador a visual studio para que el pip install no me tirara error)
 4.python .\prueba.py
+
+Para el football_kit_recognizer:
+1. pip install Pillow (Pillow es una libreria para editar/ver imagenes en python)
+
+Guia para tensorFlow:
+https://www.tensorflow.org/tutorials/images/classification?hl=es-419
